c7987ae006d26cee6f82a515f8de5b5e7349fa1e
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2023-10-23 16:16:27 +0700

    init: init module

---
+++ odoo-addons/om_hospital/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'summary': 'Hospital Management System',
+    'description': """
+This module contains all the common features of Hospital Management System.
+    """,
+    'author': "rereasdev",
+    'sequence': -100,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
