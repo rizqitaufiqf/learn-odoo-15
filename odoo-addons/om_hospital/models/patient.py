1938c7d444909d93d92eb22bc2eaba20f9d78a47
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2023-10-24 13:48:41 +0700

    feat: enable log tracking change form value

--- odoo-addons/om_hospital/models/patient.py
+++ odoo-addons/om_hospital/models/patient.py
@@ -8,11 +8,11 @@ class HospitalPatient(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Hospital Patient'
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Name', tracking=True)
     ref = fields.Char(string='Reference')
-    age = fields.Integer(string='Age', group_operator='avg')
+    age = fields.Integer(string='Age', tracking=True, group_operator='avg')
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female'),
-    ], string='Gender')
+    ], string='Gender', tracking=True)
     active = fields.Boolean(string='Active', default=True)
