cfc43189ea743486fb9eb5c73aaf8bfdf4a88526
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2023-10-24 16:28:17 +0700

    feat: add related field on gender beetween patient and appointment

--- odoo-addons/om_hospital/models/appointment.py
+++ odoo-addons/om_hospital/models/appointment.py
@@ -7,6 +7,7 @@ class HospitalAppointment(models.Model):
     _description = 'Hospital Appointment'
 
     patient_id = fields.Many2one(comodel_name='hospital.patient', string='Patient')
+    gender = fields.Selection(string='Gender', related='patient_id.gender')
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Datetime(string='Booking Date', tracking=True)
 
